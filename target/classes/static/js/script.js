f142157e9e4b7e6ced7fd4b6091866cc9a859d0f
DrivenByStorm <102715674+SpecialistSteak@users.noreply.github.com>
2024-01-01 04:32:23 +0530

    updated code

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,11 @@
+function submitAssignment() {
+    console.log("submitAssignment");
+}
+
+function submitProject() {
+    console.log("submitProject");
+}
+
+function logTest() {
+    console.log("logTest");
+}
